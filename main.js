e227a6d98c26dfdc3925d37bc64da315e8f14a98
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-01-31 15:10:56 +0000

    initial js commit

--- main.js
+++ main.js
@@ -1,1 +1,53 @@
- 
+const liveGBPDisplay = document.querySelector('#live-gbp-rate');
+const liveUSDDisplay = document.querySelector('#live-usd-rate');
+
+function getRateDollarsToPounds() {
+  let rate = fetch('https://api.exchangerate-api.com/v4/latest/USD', {})
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log('$to£', data.rates.GBP);
+      liveGBPDisplay.innerText = data.rates.GBP;
+    });
+}
+const liveGBPDisplay = document.querySelector;
+
+function getRateDollarsToPounds() {
+  let rate = fetch('https://api.exchangerate-api.com/v4/latest/USD', {})
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log('$to£', data.rates.GBP);
+      return rateDollarsToPounds;
+    });
+}
+
+function getRatePoundsToDollars() {
+  let rate = fetch('https://api.exchangerate-api.com/v4/latest/GBP', {})
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log('£to$', data.rates.USD);
+      return ratePoundsToDollars;
+    });
+}
+
+getRateDollarsToPounds();
+getRatePoundsToDollars();
+
+function getRatePoundsToDollars() {
+  let rate = fetch('https://api.exchangerate-api.com/v4/latest/GBP', {})
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log('£to$', data.rates.USD);
+      liveUSDDisplay.innertext = data.rates.GBP;
+    });
+}
+
+getRateDollarsToPounds();
+getRatePoundsToDollars();
