3c807127b855722e401027a51da92e4ce4095c10
samwebber95 <samuel.webber95@gmail.com>
2020-01-31 15:16:53 +0000

    second commit

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
- 
+let mainTitle = document.createElement("h1");
+document.body.appendChild(mainTitle);
+mainTitle.innerText = "Bank of Webber & Kaufman";
+mainTitle.classList.add("typewriter1");
+
+let introPara = document.createElement("p");
+document.body.appendChild(introPara);
+introPara.innerText =
+  "Welcome to the Bank of Webber & Kaufman. You can convert your Dollars into Pounds, and vice versa, with us. Please indulge in our currency converter below.";
+introPara.classList.add("typewriter");
